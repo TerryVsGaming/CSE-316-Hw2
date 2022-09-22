473d9e21e9ff581ec223342e3efd7a068d9e7667
TerryVsGaming <Terry34life@gmail.com>
2022-09-22 12:44:57 -0400

    finished most differences

---
+++ src/components/SongCard.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+export default class SongCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isDragging: false,
+            draggedTo: false
+        }
+    }
+    handleDragStart = (event) => {
+        event.dataTransfer.setData("song", event.target.id);
+        this.setState(prevState => ({
+            isDragging: true,
+            draggedTo: prevState.draggedTo
+        }));
+    }
+    handleDragOver = (event) => {
+        event.preventDefault();
+        this.setState(prevState => ({
+            isDragging: prevState.isDragging,
+            draggedTo: true
+        }));
+    }
+    handleDragEnter = (event) => {
+        event.preventDefault();
+        this.setState(prevState => ({
+            isDragging: prevState.isDragging,
+            draggedTo: true
+        }));
+    }
+    handleDragLeave = (event) => {
+        event.preventDefault();
+        this.setState(prevState => ({
+            isDragging: prevState.isDragging,
+            draggedTo: false
+        }));
+    }
+    handleDrop = (event) => {
+        event.preventDefault();
+        let target = event.target;
+        let targetId = target.id;
+        targetId = targetId.substring(target.id.indexOf("-") + 1);
+        let sourceId = event.dataTransfer.getData("song");
+        sourceId = sourceId.substring(sourceId.indexOf("-") + 1);
+        
+        this.setState(prevState => ({
+            isDragging: false,
+            draggedTo: false
+        }));
+
+        // ASK THE MODEL TO MOVE THE DATA
+        this.props.moveCallback(sourceId, targetId);
+    }
+
+    getItemNum = () => {
+        return this.props.id.substring("playlist-song-".length);
+    }
+
+    render() {
+        const { song, selected } = this.props;
+        let num = this.getItemNum();
+        console.log("num: " + num);
+        let itemClass = "playlister-song";
+        if (this.state.draggedTo) {
+            itemClass = "playlister-song-dragged-to";
+        }
+
+        let selectClass = "unselected-list-card";
+        if (selected) {
+            selectClass = "selected-list-card";
+        }
+
+        return (
+            <div 
+                id={'song-' + num}
+                className={itemClass + " " + selectClass}
+                onDragStart={this.handleDragStart}
+                onDragOver={this.handleDragOver}
+                onDragEnter={this.handleDragEnter}
+                onDragLeave={this.handleDragLeave}
+                onDrop={this.handleDrop}
+                draggable="true"
+            >
+                {num + "."}
+                <a href={"https://www.youtube.com/watch?v=" + song.youTubeId} >
+                {song.title} by {song.artist}
+                </a>
+                <input type="button" id={'delete-song-' + num} class="list-card-button" value="X"></input>
+            </div>
+
+            
+        )
+    }
+}
